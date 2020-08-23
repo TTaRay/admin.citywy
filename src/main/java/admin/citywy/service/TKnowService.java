3bdbb30a279a43c1c9373556c29eea5b69022379
taray <1143024837@qq.com>
2020-08-23 22:00:34 +0800

    Initial commit

---
+++ src/main/java/admin/citywy/service/TKnowService.java
@@ -0,0 +1,50 @@
+package admin.citywy.service;
+
+import java.util.List;
+
+import admin.citywy.common.PageHelper;
+import admin.citywy.entity.TComment;
+import admin.citywy.entity.TKnow;
+
+public interface TKnowService {
+	/**
+     * 查询所有Content并分页
+     * @param content
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    PageHelper.Page<TKnow> findAll(TKnow content, Integer pageNum, Integer pageSize);
+    PageHelper.Page<TKnow> findAll(TKnow content, TComment comment, Integer pageNum, Integer pageSize);
+    PageHelper.Page<TKnow> findAllByUpvote(TKnow content, Integer pageNum, Integer pageSize);
+    /**
+     * 添加知识文章
+     * @param content
+     */
+    void addContent(TKnow content);
+    /**
+     * 根据用户ID查询文章
+     * @param uid 用户ID
+     * @return
+     */
+    List<TKnow> findByUserId(long uid);
+    /**
+     * 查询所有文章
+     * @return
+     */
+    List<TKnow> findAll();
+
+    /**
+     * 根据文章id查找文章
+     * @param id
+     * @return
+     */
+    TKnow findById(long id);
+    /**
+     * 根据文章id更新文章
+     * @param content
+     * @return
+     */
+    void updateById(TKnow content);
+    
+}
